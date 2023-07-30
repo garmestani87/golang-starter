ba3de9c8939735a907fa97f6d97e6aaa10a47353
m.garmestani <m.garmestani@sadad.co.ir>
2023-07-30 16:03:37 +0330

    add array

---
+++ arrays/arrays.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var i int = 0
+	for {
+		i++
+		println("hello loop")
+		if i >= 10 {
+			break
+		}
+	}
+
+	for j := 0; j <= 10; j++ {
+		println("hello loop")
+	}
+
+	lst := []int{1, 5, 8, 9}
+	for index, item := range lst {
+		fmt.Printf("index = %d , item = %d \n", index, item)
+	}
+
+}
