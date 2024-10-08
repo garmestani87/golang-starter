3e453177708f0cc0d3433fc6f961f4cef9411bd1
mohsen <mohsen.salkhordeh66@gmail.com>
2024-10-08 14:06:33 +0330

    add ctx cancel

---
+++ http/ctx-cancel.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go func() {
+		for {
+			if time.Now().Second()%9 == 0 {
+				cancel()
+			}
+		}
+	}()
+
+	go add(ctx, 1)
+	sub(ctx, 1)
+}
+
+func add(ctx context.Context, i int) {
+	for {
+		i += 1
+		select {
+		case <-ctx.Done():
+			fmt.Printf("add is cancelled ! num is : %d \n", i)
+			time.Sleep(1 * time.Second)
+			return
+		default:
+			{
+				fmt.Printf("add is processing ! num is : %d \n", i)
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}
+}
+
+func sub(ctx context.Context, i int) {
+	for {
+		i -= 1
+		select {
+		case <-ctx.Done():
+			fmt.Printf("sub is cancelled ! num is : %d", i)
+			return
+		default:
+			{
+				fmt.Printf("sub is processing ! num is : %d \n", i)
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}
+}
